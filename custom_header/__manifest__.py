b5e52c6eca222e1a7440435337268de5f77aa68b
sahil Dwivedi <sahil@sahil.dwivedi>
2019-03-29 18:39:40 +0530

    add image header

--- custom_header/__manifest__.py
+++ custom_header/__manifest__.py
@@ -3,7 +3,7 @@
     'name': "Custom Header",
 
     'summary': """
-        This module is develope to customise color of odoo. The process is done under maintance of oodles technologies.The Job is dpne our odoo 	developer.""",
+        This module is develope to customise color of odoo. The process is done under maintance of oodles technologies.The Job is done our odoo 	developer.""",
 
     'description': """
         Change color of default odoo color theme 
@@ -15,7 +15,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Theme',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
