9a12a7e8a65087af8bf22369dae0227806d41fdc
oodlesShivam <shivam@shivam.sahu>
2019-04-18 18:52:44 +0530

    Adding Custom Module For Automating Lots/Serial Number

---
+++ lot_automation/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Lot/Serial Numbers Automation",
+    'summary': """This App is used for the automation of lot number""",
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+    'category': 'Stock',
+    'version': '0.1',
+    'depends': ['base', 'stock', ],
+    'data': ['stock.picking.custom.xml',],
+    'demo': ['demo/demo.xml',],
+}
